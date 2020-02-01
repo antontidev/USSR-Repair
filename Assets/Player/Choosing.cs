6b61c6bcf1455e9845a977b50c2295a0cc298c30
pink production <sanjaplushka@gmail.com>
2020-02-01 15:23:49 +0500

    Увеличение

---
+++ Assets/Player/Choosing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choosing : MonoBehaviour
+{
+    public float scaleEffect;
+    public Transform transform;
+
+    public bool entered = false;
+    private void OnMouseEnter()
+    {
+        entered = true;
+    }
+
+    private void OnMouseExit()
+    {
+        entered = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
